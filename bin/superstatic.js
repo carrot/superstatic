e91c6d67a698996c1e025bb715939e96e2bcb7c7
tmilewski <tmilewski@gmail.com>
2014-04-01 17:41:48 -0400

    Speed up the internets with Node clusters. (v0.10.26)

--- bin/superstatic.js
+++ bin/superstatic.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+var cluster = require('cluster');
 var path = require('path');
 var chokidar = require('chokidar');
 var argv = require('minimist')(process.argv.slice(2));
@@ -8,7 +9,6 @@ var defaults = require('../lib/defaults');
 var ConfigFile = require('../lib/server/settings/file');
 var JSUN = require('jsun');
 var fs = require('fs');
-var server;
 
 // app working directory
 var port = exports.port =  argv.port || argv.p || defaults.PORT;
@@ -19,31 +19,40 @@ var awd = exports.awd = (argv._[0])
  : defaults.DIRECTORY;
 var envJSON = path.join(awd, "./.env.json");
 
-//
-startServer();
+////////////
+// MASTER //
+////////////
 
-// Watch config file for changes
-process.nextTick(function () {
-  try{
-    chokidar.watch(server.settings.getConfigFileName())
-      .on('change', configFileChanged);
+if (cluster.isMaster) {
+  console.log('Cluster: Master running ... ');
+
+  // Count the number of CPUs
+  var cpuCount = require('os').cpus().length;
+
+  // Create a worker for each CPU
+  for (var i = 0; i < cpuCount; i += 1) {
+    cluster.fork();
   }
-  catch (e) {}
-});
 
-function configFileChanged () {
-  console.log('Configuration file changed. Restarting...');
-  server.stop(startServer);
-}
+  cluster.on('exit', function(worker, code, signal) {
+    cluster.fork();
 
-function startServer () {
-  server = createInstance(awd, host, port);
-  server.start(function () {
-    preamble(host, port);
+    if (worker.suicide === true) {
+      console.log('Cluster: Worker ' + worker.id + ' was intentionally killed.');
+    } else {
+      console.log('Cluster: Worker ' + worker.id + ' died.');
+    }
   });
 }
 
-function createInstance (awd, host, port) {
+////////////
+// WORKER //
+////////////
+
+if (cluster.isWorker) {
+  console.log('Cluster: Worker ' + cluster.worker.id + ' running ... ');
+
+  // Set configuration options
   var configOptions = (overrideConfig)
     ? {
         config: overrideConfig,
@@ -58,7 +67,8 @@ function createInstance (awd, host, port) {
       var localEnv = JSON.parse(fs.readFileSync(envJSON));
     }
 
-  return Superstatic.createServer({
+  // Create server
+  server = Superstatic.createServer({
     port: port,
     host: host,
     settings: new ConfigFile(configOptions),
@@ -70,22 +80,23 @@ function createInstance (awd, host, port) {
       }
     }
   });
-};
 
-function preamble (host, port) {
-  console.log('Server started on port ' + port.toString());
-  console.log('');
-}
+  // Alert on start
+  server.start(function () {
+    console.log('Server started on port ' + port.toString());
+  });
 
-function postamble (evt, filePath) {
-  console.log('');
-  console.log(evt.green + ': ' + filePath);
-  process.stdout.write('Reconfiguring server ... '.yellow);
-}
-
-function doneabmle () {
-  process.stdout.write('done'.blue);
-  console.log('\n\nListening for changes...');
+  // Kill (and restart) the worker if the configuation changes
+  process.nextTick(function () {
+    try{
+      chokidar.watch(server.settings.getConfigFileName())
+        .on('change', function() {
+          console.log('Configuration file changed. Restarting worker...');
+          cluster.worker.kill()
+        });
+    }
+    catch (e) {}
+  });
 }
 
 function parseOverrideConfig (argv) {
